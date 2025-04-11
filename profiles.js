0fb219c0eb62363424a0fc7f7299c31c90ad98b5
anumbys <aleksej.spiridonov@my.com>
2025-04-11 21:14:19 +0300

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    window.profiles_settings = {
+        profiles: [
+            {
+              id: '',
+              name: 'Profile 1',
+              params: {
+                adult: true,
+              },
+            },
+            {
+              id: 'profile_2',
+              icon: 'https://cdn.cub.red/img/profiles/f_2.png'
+            }
+       ]
+    };
+
+    Lampa.Utils.putScript(['https://levende.github.io/lampa-plugins/profiles.js'], function() {});
+})();
