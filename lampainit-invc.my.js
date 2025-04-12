b85b3d10a7db54de9a13bf0ad13e06b8c366659c
anumbys <aleksej.spiridonov@my.com>
2025-04-12 19:32:18 +0300

    Update lampainit-invc.my.js

--- lampainit-invc.my.js
+++ lampainit-invc.my.js
@@ -42,19 +42,6 @@ window.lampainit_invc = {};
 
 
 // Лампа готова для использования
-window.lampainit_invc.appload = function appload() {
-Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks", "https://bylampa.github.io/account.js", "https://bylampa.github.io/source.js", "https://bylampa.github.io/backmenu.js", "https://bylampa.github.io/seas_and_eps.js", "http://bwa.to/rc/889yfnh", "https://bwa.to/r", "https://levende.github.io/lampa-plugins/custom-favs.js", "https://levende.github.io/lampa-plugins/lampac-src-filter.js", "https://aviamovie.github.io/surs.js", "https://apxubatop.github.io/lmpPlugs/tvbutton.js", "https://anumbys.github.io/lampa/notextend.js", "https://BDVBurik.github.io/rezkacomment.js"]);
-// (, "https://lampame.github.io/main/pubtorr/pubtorr.js")
-    Lampa.Storage.set('parser_use', 'true');
-    Lampa.Storage.set('jackett_url', 'https://jacred.xyz');
-//    Lampa.Storage.set('jackett_key', '1');
-    Lampa.Storage.set('parser_torrent_type', 'jackett');
-    Lampa.Storage.set('parse_in_search', 'true');
-//    Lampa.Storage.set('torrserver_url','https://ts.maxvol.pro'),
-    Lampa.Storage.set('internal_torrclient', 'true');
-//    Lampa.Storage.set('surs_disableCustomName', true);
-    Lampa.Storage.set('surs_name', 'MIX');
-}
 
 
 //// Лампа полностью загружена, можно работать с интерфейсом   ( ne rabotaet)
@@ -166,6 +153,14 @@ window.lampainit_invc.first_initiale = function firstinitiale() {
     Lampa.Storage.set('video_quality_default', '2160');
     Lampa.Storage.set('poster_size', 'w500');
 
+Lampa.Storage.set('start_page', 'last');
+
+
+Lampa.Storage.set('menu_sort', JSON.stringify([ "Главная", "Избранное", "История", "Торренты", "Фильмы", "Аниме", "Сериалы"]));
+
+Lampa.Storage.set('menu_hide', JSON.stringify(["Расписание", "Релизы", "Подписки",]));
+
+Lampa.Storage.set('torrents_sort', 'size');
 
 //    var plugins = Lampa.Plugins.get();
 
