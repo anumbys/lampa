40950161de295cfe1139969f1d88741f35f7d08a
anumbys <aleksej.spiridonov@my.com>
2025-04-12 21:00:43 +0300

    Update lampainit-invc.my.js

--- lampainit-invc.my.js
+++ lampainit-invc.my.js
@@ -62,7 +62,7 @@ Lampa.Storage.set('start_page', 'last');
 
 Lampa.Storage.set('menu_sort', JSON.stringify([ "Главная", "Избранное", "История","Торренты", "Фильмы", "Аниме", "Сериалы", "MIX RUS"]));
 
-Lampa.Storage.set('menu_hide', JSON.stringify([ "Расписание", "Релизы"]));
+Lampa.Storage.set('menu_hide', JSON.stringify([ "Расписание", "Релизы", "MIX"]));
 
 Lampa.Storage.set('surs_buttons', '7');
 Lampa.Storage.set('torrents_sort', 'size');
