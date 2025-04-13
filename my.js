d7b7665ac8756994883978bdb74a330540cf6be7
anumbys <aleksej.spiridonov@my.com>
2025-04-13 11:06:09 +0300

    Update my.js

--- my.js
+++ my.js
@@ -33,9 +33,8 @@ window.lampainit_invc = {};
 
 
 window.lampainit_invc.appload = function appload() {
-// удалить все плагины которые установлены в памяти устройства?
-// Потому что теперь они грузятся через лампаинит, хочу удалить сразу все на всех устройствах
-Lampa.Storage.set('plugins', '["https://bylampa.github.io/tmdb-proxy.js"]')
+// Удалить все плагины которые установлены в памяти устройства. При таком случае если лампа и плагин на одном сервере
+// Lampa.Storage.set('plugins', '["https://bylampa.github.io/tmdb-proxy.js"]')
 // Добовляем плагины, но без возможности отключить (в плагинах отображаться не будет)
 Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks", "https://bylampa.github.io/account.js", "https://bylampa.github.io/source.js", "https://bylampa.github.io/backmenu.js", "https://bylampa.github.io/seas_and_eps.js", "https://levende.github.io/lampa-plugins/custom-favs.js", "https://levende.github.io/lampa-plugins/lampac-src-filter.js", "https://aviamovie.github.io/surs.js", "https://apxubatop.github.io/lmpPlugs/tvbutton.js", "https://anumbys.github.io/lampa/notextend.js", "https://BDVBurik.github.io/rezkacomment.js"], function() {});
 
