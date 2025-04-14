a66f75ad60866c0f86672bd6911e882e3a9cf6de
anumbys <aleksej.spiridonov@my.com>
2025-04-14 18:15:24 +0300

    Update my.js

--- my.js
+++ my.js
@@ -35,7 +35,7 @@ window.lampainit_invc.appload = function appload() {
 // Lampa.Storage.set('plugins', '["https://bylampa.github.io/tmdb-proxy.js"]')
 // Добовляем плагины, но без возможности отключить (в плагинах отображаться не будет)
 Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks", "https://bylampa.github.io/account.js", "https://bylampa.github.io/source.js", "https://bylampa.github.io/backmenu.js", "https://bylampa.github.io/seas_and_eps.js" , "https://aviamovie.github.io/surs.js", "https://levende.github.io/lampa-plugins/custom-favs.js", "https://levende.github.io/lampa-plugins/lampac-src-filter.js", "https://apxubatop.github.io/lmpPlugs/tvbutton.js", "https://anumbys.github.io/lampa/notextend.js", "https://BDVBurik.github.io/rezkacomment.js"], function() {});
-// , "https://aviamovie.github.io/surs.js"
+
     // Добовляем плагины по умолчанию, чтобы руками не вписывать.
 var plugins = Lampa.Plugins.get();
 
