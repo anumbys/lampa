9787a1f33dc63b3debee8551c115416ebebd0719
anumbys <aleksej.spiridonov@my.com>
2025-04-14 18:29:28 +0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -14,12 +14,12 @@
             {
               id: 'profile_2',
               title: '2',
-              icon: 'https://cdn.ub.red/img/profiles/l_4.png'
+              icon: 'https://cdn.cub.red/img/profiles/l_4.png'
             },
             {
               id: 'profile_3',
               title: '3',
-              icon: 'https://cdn.cub.red/img/profiles/f_2.png'
+              icon: 'https://cdn.cub.red/img/profiles/l_3.png'
             },
             {
               id: 'profile_4',
