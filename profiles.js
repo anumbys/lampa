71e29fe06417bbb9911a66968d1dcdd7ea6932be
anumbys <aleksej.spiridonov@my.com>
2025-04-14 18:26:13 +0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -14,22 +14,22 @@
             {
               id: 'profile_2',
               title: '2',
-              icon: 'https://cdn.cub.red/img/profiles/f_2.png'
+              icon: 'https://cdn.ub.red/img/profiles/l_4.png'
             },
             {
               id: 'profile_3',
               title: '3',
-              icon: 'https://cdn.cub.red/img/profiles/f_1.png'
+              icon: 'https://cdn.cub.red/img/profiles/f_2.png'
             },
             {
               id: 'profile_4',
               title: '4',
-              icon: 'https://cub.red/img/profiles/l_3.png'
+              icon: 'https://cdn.cub.red/img/profiles/l_5.png'
             },
             {
               id: 'profile_5',
               title: '5',
-              icon: 'https://cub.red/img/profiles/l_5.png'
+              icon: 'https://cdn.cub.red/img/profiles/l_6.png'
             }
        ]
     };
