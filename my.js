b517f2336a24c7b1c14e71cabebfb91c39718024
anumbys <aleksej.spiridonov@my.com>
2025-04-22 21:33:33 +0300

    Update my.js

--- my.js
+++ my.js
@@ -153,35 +153,5 @@ var styleElement = document.createElement('style');
   }, 200);
 // Удаляю компоненты END
 
-  function start() {		
-
-
-
-
-
-
-    Lampa.Storage.set('source', 'MIX');    //    Источник по умолчанию
-Lampa.Storage.set('start_page', 'last');  // Стартовая страница
-Lampa.Storage.set('menu_sort', ([ "Главная", "Избранное", "История","Торренты", "Фильмы", "Аниме", "Сериалы", "Радио"]));  // Cортировка меню
-Lampa.Storage.set('menu_hide', ([ "Расписание", "Релизы"]));
-Lampa.Storage.set('torrents_sort', 'size');
-
-//// Лампа полностью загружена, можно работать с интерфейсом
-    Lampa.Storage.set('lampac_initiale', 'true');
-//    Lampa.Storage.set('video_quality_default', '2160');
-    Lampa.Storage.set('poster_size', 'w500');
-
-
-
-	
-    if (window.lampainit_invc)
-      window.lampainit_invc.first_initiale();
-
-    /*
-    setTimeout(function(){
-        Lampa.Noty.show('Плагины установлены, перезагрузка через 5 секунд.',{time: 5000})
-    },5000)
-    */
-  }
 
 })();
