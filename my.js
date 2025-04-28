e75e8214fcf1f042ebd31cea40644280656fa1ef
anumbys <aleksej.spiridonov@my.com>
2025-04-28 21:05:24 +0300

    Update my.js

--- my.js
+++ my.js
@@ -36,7 +36,32 @@
 Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks","https://bylampa.github.io/account.js","https://bylampa.github.io/source.js","https://bylampa.github.io/backmenu.js","https://bylampa.github.io/seas_and_eps.js","https://aviamovie.github.io/surs.js","https://levende.github.io/lampa-plugins/custom-favs.js","https://levende.github.io/lampa-plugins/lampac-src-filter.js","https://apxubatop.github.io/lmpPlugs/tvbutton.js","https://anumbys.github.io/lampa/notextend.js","https://BDVBurik.github.io/rezkacomment.js","http://bwa.to/rc/889yfnh","https://bwa.to/r"]);
 
 
+    // Добовляем плагины по умолчанию, чтобы руками не вписывать.
+var plugins = Lampa.Plugins.get();
 
+var plugins_add = [
+//{"url": "https://anumbys.github.io/lampa/profiles.js","status": 0,"name": "Профили","author": "lampac"},
+{"url": "https://kartmansms.github.io/lampa/Shikimori/Shikimori.js","status": 0,"name": "LME Shikimori Mod","author": "lampac"},
+{"url": "https://lampame.github.io/main/MovieEnhancer/MovieEnhancer.js","status": 0,"name": "Доп. инфо в карточке","author": "lampac"},
+{"url": "https://lampame.github.io/main/nc/nc.js","status": 0,"name": "Дополнительные категории","author": "lampac"}
+//{"url": "https://lampame.github.io/main/pubtorr/pubtorr.js","status": 0,"name": "Публичные парсеры","author": "lampac"}
+];
+
+var plugins_push = []
+
+plugins_add.forEach(function (plugin) {
+    if (!plugins.find(function (a) {
+        return a.url == plugin.url
+    })) {
+        Lampa.Plugins.add(plugin);
+        Lampa.Plugins.save();
+
+        plugins_push.push(plugin.url)
+    }
+});
+
+if (plugins_push.length) Lampa.Utils.putScript(plugins_push, function () { }, function () { }, function () { }, true);
+    // Добовляем плагины по умолчанию, чтобы руками не вписывать. END
 
 // Jacket and Torrserver
     Lampa.Storage.set('parser_use', 'true');
