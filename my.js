00818d25eeb4b02c50f64ac1bf0a37f8f2851754
anumbys <aleksej.spiridonov@my.com>
2025-04-28 19:08:56 +0300

    Update my.js

--- my.js
+++ my.js
@@ -97,43 +97,6 @@ Lampa.Storage.set('menu_hide', ([ "Расписание", "Релизы"]));
 //   Lampa.Storage.set('source', 'MIX');
 //}
 
-// Скрыть меню в настройках - Синхронизация, Парсер (, 'parser'), TorrServer (, 'server'), IPTV, Расширения, TMDB
-    Lampa.Settings.listener.follow('open', function(e) {
-      $(['account', 'parser', 'iptv', 'tmdb', 'parental_control'].map(function(c) {
-        return '[data-component="' + c + '"]';
-      }).join(','), e.body).remove();
-
-
-      if (e.name == 'interface') {
-        e.body.find('[data-name="light_version"]').remove();
-//        e.body.find('[data-name="card_interfice_type"]').remove();  // CUB
-//        e.body.find('[data-name="card_interfice_reactions"]').remove();  // CUB
-      }
-
-      if (e.name == 'more') {
-        e.body.find('[data-name="cache_images"]').remove();
-        e.body.find('[data-name="device_name"]').remove();
-        e.body.find('[data-name="export"]').remove();
-      }
-    });
-
-    // Скрыть разделы в меню
-    Lampa.Listener.follow('app', function(e) {
-      if (e.type === 'ready') {
-        $("[data-action=feed]").hide();        // лента
-        $("[data-action=myperson]").hide();    // cub подписка на актеров
-        $("[data-action=subscribes]").hide();  // cub подписки
-//        $("[data-action=mytorrents]").hide();
-        $("[data-action=about]").hide();
-        $("[data-action=console]").hide();
-        $("[data-action=timetable]").hide();
-      }
-    });
-
-// убрать с интерфейса колокольчик и звёздочку
-var styleElement = document.createElement('style');
-    styleElement.innerHTML = '.head .notice--icon { display: none; }';
-    document.body.appendChild(styleElement);
 
 
 
