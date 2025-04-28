f06e437f7980a5b2b58df5e1a840891cba8ad2ea
anumbys <aleksej.spiridonov@my.com>
2025-04-28 18:27:43 +0300

    Update my.js

--- my.js
+++ my.js
@@ -73,12 +73,22 @@ if (plugins_push.length) Lampa.Utils.putScript(plugins_push, function () { }, fu
 //    Lampa.Storage.set('torrserver_url',''),
     Lampa.Storage.set('internal_torrclient', 'true'); // включенный встроенный клиент торсервера
 
+//    Lampa.Storage.set('surs_disableCustomName', true);
+    Lampa.Storage.set('surs_name', 'MIX'); // Название источника AVIAMOVIE
+    Lampa.Storage.set('source', 'MIX');    //    Источник по умолчанию
+// Постояные настроики, после изминений и перезагрузке настроики перключаются обратно на эти первоначалные
+//   "function start() {}" не работает
+    Lampa.Storage.set('lampac_initiale', 'true');
+      Lampa.Storage.set('animation', 'false');               // Анимация отключена
+      Lampa.Storage.set('protocol', 'http');                 // cub api протокол http/https
+Lampa.Storage.set('start_page', 'last');  // Стартовая страница
+Lampa.Storage.set('menu_sort', ([ "Главная", "Избранное", "История","Торренты", "Фильмы", "Аниме", "Сериалы", "Радио"]));  // Cортировка меню
+Lampa.Storage.set('menu_hide', ([ "Расписание", "Релизы"]));
 // Skrypt error
 //Lampa.Storage.set('torrents_sort', 'size');
 
 //// Лампа полностью загружена, можно работать с интерфейсом
 //    Lampa.Storage.set('video_quality_default', '2160');    // Настройки, плеер, качество видео по умолчанию 2160/1080/720
-    Lampa.Storage.set('poster_size', 'w500');
 }
 
 // Выполняется один раз, когда пользователь впервые открывает лампу
