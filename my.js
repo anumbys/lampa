7b2c3525e86f23d3cf89d8cecfbc2d7440c6f888
anumbys <aleksej.spiridonov@my.com>
2025-07-17 13:49:50 +0300

    Update my.js

--- my.js
+++ my.js
@@ -27,27 +27,26 @@
     persons: true        // off
   }
 
-window.lampainit_invc = {};
 
-// Лампа готова для использования
-window.lampainit_invc.appload = function appload() {
 // Добовляем плагины, но без возможности отключить (в плагинах отображаться не будет)
 // ,"https://apxubatop.github.io/lmpPlugs/tvbutton.js"
 // Удалить все плагины которые установлены в памяти устройства. Применять только когда лампа и плагин на одном сервере
 // Scrypt error
 // Lampa.Storage.set('plugins', '["https://bylampa.github.io/tmdb-proxy.js"]')
-Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks","https://bylampa.github.io/account.js","https://bylampa.github.io/source.js","https://bylampa.github.io/backmenu.js","https://bylampa.github.io/seas_and_eps.js","https://aviamovie.github.io/surs.js","https://levende.github.io/lampa-plugins/custom-favs.js","https://levende.github.io/lampa-plugins/lampac-src-filter.js","https://BDVBurik.github.io/rezkacomment.js","http://bwa.to/rc/889yfnh","https://bwa.to/r","https://anumbys.github.io/lampa/notextend.js"]);
+Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks","https://bylampa.github.io/account.js","https://bylampa.github.io/source.js","https://bylampa.github.io/backmenu.js","https://bylampa.github.io/seas_and_eps.js","https://aviamovie.github.io/surs.js","https://levende.github.io/lampa-plugins/custom-favs.js","https://levende.github.io/lampa-plugins/lampac-src-filter.js","https://anumbys.github.io/lampa/notextend.js","https://BDVBurik.github.io/rezkacomment.js","http://bwa.to/rc/889yfnh","https://bwa.to/r"]);
 
-// Постояные настроики, после изминений и перезагрузке настроики перключаются обратно на эти первоначалные
-// Добовляем плагины по умолчанию, чтобы руками не вписывать.
+
+
+
+    // Добовляем плагины по умолчанию, чтобы руками не вписывать.
 var plugins = Lampa.Plugins.get();
 
 var plugins_add = [
+//{"url": "https://anumbys.github.io/lampa/profiles.js","status": 0,"name": "Профили","author": "lampac"},
+{"url": "https://apxubatop.github.io/lmpPlugs/tvbutton.js","status": 1,"name": "tvbutton","author": "lampac"},
 {"url": "https://kartmansms.github.io/lampa/Shikimori/Shikimori.js","status": 0,"name": "LME Shikimori Mod","author": "lampac"},
 {"url": "https://lampame.github.io/main/MovieEnhancer/MovieEnhancer.js","status": 0,"name": "Доп. инфо в карточке","author": "lampac"},
 {"url": "https://lampame.github.io/main/nc/nc.js","status": 0,"name": "Дополнительные категории","author": "lampac"}
-//{"url": "https://anumbys.github.io/lampa/profiles.js","status": 0,"name": "Профили","author": "lampac"},
-//{"url": "https://apxubatop.github.io/lmpPlugs/tvbutton.js","status": 1,"name": "tvbutton","author": "lampac"},
 //{"url": "https://lampame.github.io/main/pubtorr/pubtorr.js","status": 0,"name": "Публичные парсеры","author": "lampac"}
 ];
 
@@ -69,7 +68,7 @@ if (plugins_push.length) Lampa.Utils.putScript(plugins_push, function () { }, fu
 
 // Jacket and Torrserver
     Lampa.Storage.set('parser_use', 'true');
-    Lampa.Storage.set('jackett_url', 'jacred.xyz');
+    Lampa.Storage.set('jackett_url', 'https://jacred.xyz');
 //    Lampa.Storage.set('jackett_key', '1');
     Lampa.Storage.set('parser_torrent_type', 'jackett');
     Lampa.Storage.set('parse_in_search', 'true');
@@ -77,22 +76,28 @@ if (plugins_push.length) Lampa.Utils.putScript(plugins_push, function () { }, fu
     Lampa.Storage.set('internal_torrclient', 'true'); // включенный встроенный клиент торсервера
 
 //    Lampa.Storage.set('surs_disableCustomName', true);
-Lampa.Storage.set('surs_name', 'MIX'); // Название источника AVIAMOVIE
-Lampa.Storage.set('source', 'MIX');    //    Источник по умолчанию
-Lampa.Storage.set('lampac_initiale', 'true');
-Lampa.Storage.set('animation', 'false');               // Анимация отключена
-Lampa.Storage.set('protocol', 'http');                 // cub api протокол http/https
+    Lampa.Storage.set('surs_name', 'MIX'); // Название источника AVIAMOVIE
+    Lampa.Storage.set('source', 'MIX');    //    Источник по умолчанию
+// Постояные настроики, после изминений и перезагрузке настроики перключаются обратно на эти первоначалные
+//   "function start() {}" не работает
+    Lampa.Storage.set('lampac_initiale', 'true');
+      Lampa.Storage.set('animation', 'false');               // Анимация отключена
+      Lampa.Storage.set('protocol', 'http');                 // cub api протокол http/https
 Lampa.Storage.set('start_page', 'last');  // Стартовая страница
 Lampa.Storage.set('menu_sort', ([ "Главная", "Избранное", "История","Торренты", "Фильмы", "Аниме", "Сериалы", "Радио"]));  // Cортировка меню
 Lampa.Storage.set('menu_hide', ([ "Расписание", "Релизы"]));
 // Skrypt error
 //Lampa.Storage.set('torrents_sort', 'size');
 
+//// Лампа полностью загружена, можно работать с интерфейсом
 //    Lampa.Storage.set('video_quality_default', '2160');    // Настройки, плеер, качество видео по умолчанию 2160/1080/720
-Lampa.Storage.set('poster_size', 'w500');
+    Lampa.Storage.set('poster_size', 'w500');
 
-// Постояные настроики END
-}
+
+// Выполняется один раз, когда пользователь впервые открывает лампу
+//window.lampainit_invc.first_initiale = function firstinitiale() {
+//   Lampa.Storage.set('source', 'MIX');
+//}
 
 // Скрыть меню в настройках - Синхронизация, Парсер (, 'parser'), TorrServer (, 'server'), IPTV, Расширения, TMDB
     Lampa.Settings.listener.follow('open', function(e) {
@@ -134,27 +139,9 @@ var styleElement = document.createElement('style');
     styleElement.innerHTML = '.head .notice--icon { display: none; }';
     document.body.appendChild(styleElement);
 
-  var timer = setInterval(function() {
-    if (typeof Lampa !== 'undefined') {
-      clearInterval(timer);
-
-      if (window.lampainit_invc)
-        window.lampainit_invc.appload();
-
-      if (false)
-        Lampa.Storage.set('full_btn_priority', '966825172');
-
-      var unic_id = Lampa.Storage.get('lampac_unic_id', '');
-      if (!unic_id) {
-        unic_id = Lampa.Utils.uid(8).toLowerCase();
-        Lampa.Storage.set('lampac_unic_id', unic_id);
-      }
 
 
-    }
-  }, 200);
 
-// не работает
 //function start() {
 
 //    if (window.lampainit_invc)
