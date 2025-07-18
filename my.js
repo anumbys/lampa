59e93ef541afef8b8ffe2850df5543b95b8dd192
anumbys <aleksej.spiridonov@my.com>
2025-07-18 13:28:43 +0300

    Update my.js

--- my.js
+++ my.js
@@ -31,41 +31,9 @@ window.lampainit_invc = {};
 
 // Лампа готова для использования
 window.lampainit_invc.appload = function appload() {
-// Добовляем плагины, но без возможности отключить (в плагинах отображаться не будет)
-// ,"https://apxubatop.github.io/lmpPlugs/tvbutton.js"
-// Удалить все плагины которые установлены в памяти устройства. Применять только когда лампа и плагин на одном сервере
-// Scrypt error
-// Lampa.Storage.set('plugins', '["https://bylampa.github.io/tmdb-proxy.js"]')
-Lampa.Utils.putScriptAsync(["https://cub.red/plugin/tracks","https://bylampa.github.io/account.js","https://bylampa.github.io/source.js","https://bylampa.github.io/backmenu.js","https://bylampa.github.io/seas_and_eps.js","https://aviamovie.github.io/surs.js","https://levende.github.io/lampa-plugins/custom-favs.js","https://levende.github.io/lampa-plugins/lampac-src-filter.js","https://BDVBurik.github.io/rezkacomment.js","http://bwa.to/rc/889yfnh","https://bwa.to/r","https://anumbys.github.io/lampa/notextend.js"]);
+
 
 // Постояные настроики, после изминений и перезагрузке настроики перключаются обратно на эти первоначалные
-// Добовляем плагины по умолчанию, чтобы руками не вписывать.
-var plugins = Lampa.Plugins.get();
-
-var plugins_add = [
-{"url": "https://kartmansms.github.io/lampa/Shikimori/Shikimori.js","status": 0,"name": "LME Shikimori Mod","author": "lampac"},
-{"url": "https://lampame.github.io/main/MovieEnhancer/MovieEnhancer.js","status": 0,"name": "Доп. инфо в карточке","author": "lampac"},
-{"url": "https://lampame.github.io/main/nc/nc.js","status": 0,"name": "Дополнительные категории","author": "lampac"}
-//{"url": "https://anumbys.github.io/lampa/profiles.js","status": 0,"name": "Профили","author": "lampac"},
-//{"url": "https://apxubatop.github.io/lmpPlugs/tvbutton.js","status": 1,"name": "tvbutton","author": "lampac"},
-//{"url": "https://lampame.github.io/main/pubtorr/pubtorr.js","status": 0,"name": "Публичные парсеры","author": "lampac"}
-];
-
-var plugins_push = []
-
-plugins_add.forEach(function (plugin) {
-    if (!plugins.find(function (a) {
-        return a.url == plugin.url
-    })) {
-        Lampa.Plugins.add(plugin);
-        Lampa.Plugins.save();
-
-        plugins_push.push(plugin.url)
-    }
-});
-
-if (plugins_push.length) Lampa.Utils.putScript(plugins_push, function () { }, function () { }, function () { }, true);
-    // Добовляем плагины по умолчанию, чтобы руками не вписывать. END
 
 // Jacket and Torrserver
     Lampa.Storage.set('parser_use', 'true');
@@ -81,12 +49,10 @@ Lampa.Storage.set('surs_name', 'MIX'); // Название источника AV
 Lampa.Storage.set('source', 'MIX');    //    Источник по умолчанию
 Lampa.Storage.set('lampac_initiale', 'true');
 Lampa.Storage.set('animation', 'false');               // Анимация отключена
-Lampa.Storage.set('protocol', 'https');                 // cub api протокол http/https
+Lampa.Storage.set('protocol', 'http');                 // cub api протокол http/https
 Lampa.Storage.set('start_page', 'last');  // Стартовая страница
 Lampa.Storage.set('menu_sort', ([ "Главная", "Избранное", "История","Торренты", "Фильмы", "Аниме", "Сериалы", "Радио"]));  // Cортировка меню
 Lampa.Storage.set('menu_hide', ([ "Расписание", "Релизы"]));
-// Skrypt error
-//Lampa.Storage.set('torrents_sort', 'size');
 
 //    Lampa.Storage.set('video_quality_default', '2160');    // Настройки, плеер, качество видео по умолчанию 2160/1080/720
 Lampa.Storage.set('poster_size', 'w500');
