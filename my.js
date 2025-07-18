04b23e2e8e3088e749d46fbda21589e9a183c53b
anumbys <aleksej.spiridonov@my.com>
2025-07-18 13:05:12 +0300

    Update my.js

--- my.js
+++ my.js
@@ -70,6 +70,39 @@ if (plugins_push.length) Lampa.Utils.putScript(plugins_push, function () { }, fu
 
 }
 
+// Скрыть меню в настройках - Синхронизация, Парсер (, 'parser'), TorrServer (, 'server'), IPTV, Расширения, TMDB
+    Lampa.Settings.listener.follow('open', function(e) {
+      $(['account', 'parser', 'iptv', 'tmdb', 'parental_control'].map(function(c) {
+        return '[data-component="' + c + '"]';
+      }).join(','), e.body).remove();
+    });
+
+
+
+// убрать с интерфейса колокольчик и звёздочку
+var styleElement = document.createElement('style');
+    styleElement.innerHTML = '.head .notice--icon { display: none; }';
+    document.body.appendChild(styleElement);
+
+  var timer = setInterval(function() {
+    if (typeof Lampa !== 'undefined') {
+      clearInterval(timer);
+
+      if (window.lampainit_invc)
+        window.lampainit_invc.appload();
+
+      if (false)
+        Lampa.Storage.set('full_btn_priority', '966825172');
+
+      var unic_id = Lampa.Storage.get('lampac_unic_id', '');
+      if (!unic_id) {
+        unic_id = Lampa.Utils.uid(8).toLowerCase();
+        Lampa.Storage.set('lampac_unic_id', unic_id);
+      }
+
+
+    }
+  }, 200);
 
 // не работает
 //function start() {
