5028feededf3d68cfb328a5adf944a4551736324
anumbys <aleksej.spiridonov@my.com>
2025-07-23 19:51:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
- function () {
+(function () {
   'use strict';
+  
 // Jacket and Torrserver
   Lampa.Storage.set('parser_use', 'true');
   Lampa.Storage.set('jackett_url', 'https://jacred.xyz');
@@ -14,6 +15,7 @@
   Lampa.Storage.set('menu_sort', ([ "Главная", "Избранное", "История","Торренты", "Фильмы", "Аниме", "Сериалы", "Радио"]));  // Cортировка меню
   Lampa.Storage.set('menu_hide', ([ "Расписание", "Релизы"]));
 // Skrypt error
-//  Lampa.Storage.set('torrents_sort', 'size');
+  Lampa.Storage.set('torrents_sort', 'size');
   Lampa.Storage.set('poster_size', 'w500');
+  
   })();
